dc780f21444192ecef5f979c9f08eb4c647b6029
evan-metaseed <evan@metaseedlabs.com>
2024-07-04 11:15:38 -0600

    update to whitelist version

--- contracts/companion.sol
+++ contracts/companion.sol
@@ -5,17 +5,23 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./ERC721Common.sol";
 
 contract Companion is ERC721Common, ReentrancyGuard, Ownable {
     uint256 public MAX_SUPPLY = 10000;
     uint256 public PRICE = 0.001 ether;
+    uint256 public PRESALE_PRICE = 0.001 ether;
     address public withdrawAddress = 0xB4decde4c94Dc19978713D618BDf8fB6d2df6880;
     uint256 public MAX_MINT_PER_WALLET = 5;
+    uint256 public MAX_MINT_PER_WALLET_PRESALE = 5;
 
     bool public publicActive = false;
+    bool public presaleActive = false;
+    bytes32 public preSaleRoot;
 
     mapping(address => uint256) public mintsPerWallet;
+    mapping(address => uint256) public presaleMintsPerWallet;
 
     constructor(
         string memory name,
@@ -29,9 +35,13 @@ contract Companion is ERC721Common, ReentrancyGuard, Ownable {
         emit Received(msg.sender, msg.value);
     }
 
-    //setters
+    // Setters
     function setActive(bool isActive) external onlyOwner {
         publicActive = isActive;
+    }
+
+    function setPresaleActive(bool isActive) external onlyOwner {
+        presaleActive = isActive;
     }
 
     function setSupply(uint256 _supply) external onlyOwner {
@@ -42,6 +52,10 @@ contract Companion is ERC721Common, ReentrancyGuard, Ownable {
         PRICE = _price;
     }
 
+    function setPresalePrice(uint256 _price) external onlyOwner {
+        PRESALE_PRICE = _price;
+    }
+
     function setWithdrawAddress(address _address) external onlyOwner {
         withdrawAddress = _address;
     }
@@ -50,7 +64,15 @@ contract Companion is ERC721Common, ReentrancyGuard, Ownable {
         MAX_MINT_PER_WALLET = _maxMint;
     }
 
-    // Internal for marketing, devs, etc
+    function setMaxMintPerWalletPresale(uint256 _maxMint) external onlyOwner {
+        MAX_MINT_PER_WALLET_PRESALE = _maxMint;
+    }
+
+    function setPresaleRoot(bytes32 _root) external onlyOwner {
+        preSaleRoot = _root;
+    }
+
+    // Internal mint for marketing, devs, etc.
     function internalMint(uint256 quantity, address to)
         external
         onlyOwner
@@ -58,31 +80,31 @@ contract Companion is ERC721Common, ReentrancyGuard, Ownable {
     {
         require(
             totalSupply() + quantity <= MAX_SUPPLY,
-            "would exceed max supply"
+            "Would exceed max supply"
         );
         for (uint256 i = 0; i < quantity; i++) {
             _safeMint(to, totalSupply() + 1);
         }
     }
 
-    // airdrop
+    // Airdrop
     function airdrop(address[] calldata _addresses) external onlyOwner {
-        require(totalSupply() + _addresses.length <= MAX_SUPPLY, "would exceed max supply");
+        require(totalSupply() + _addresses.length <= MAX_SUPPLY, "Would exceed max supply");
         for (uint256 i = 0; i < _addresses.length; i++) {
-            require(_addresses[i] != address(0), "cannot send to 0 address");
+            require(_addresses[i] != address(0), "Cannot send to 0 address");
             _safeMint(_addresses[i], totalSupply() + 1);
         }
     }
 
-    // public mint
+    // Public mint
     function publicMint(uint8 quantity, address to)
         external
         payable
         nonReentrant
     {
         require(quantity > 0, "Must mint more than 0 tokens");
-        require(publicActive, "public sale has not begun yet");
-        require(totalSupply() + quantity <= MAX_SUPPLY, "reached max supply");
+        require(publicActive, "Public sale has not begun yet");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Reached max supply");
         require(PRICE * quantity == msg.value, "Incorrect funds");
         require(mintsPerWallet[to] + quantity <= MAX_MINT_PER_WALLET, "Minting exceeds wallet limit");
 
@@ -93,14 +115,39 @@ contract Companion is ERC721Common, ReentrancyGuard, Ownable {
         mintsPerWallet[to] += quantity;
     }
 
-    // withdraw to owner wallet
+    // Presale mint
+    function presaleMint(
+        uint8 quantity,
+        bytes32[] calldata _merkleProof
+    ) external payable nonReentrant {
+        require(presaleActive, "Presale is not active");
+        require(quantity > 0, "Must mint more than 0 tokens");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Purchase would exceed max supply of Tokens");
+        require(PRESALE_PRICE * quantity == msg.value, "Incorrect funds");
+        require(presaleMintsPerWallet[msg.sender] + quantity <= MAX_MINT_PER_WALLET_PRESALE, "Minting exceeds presale wallet limit");
+
+        // Check proof & mint
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(_merkleProof, preSaleRoot, leaf),
+            "Invalid MerkleProof"
+        );
+
+        for (uint256 i = 0; i < quantity; i++) {
+            _safeMint(msg.sender, totalSupply() + 1);
+        }
+
+        presaleMintsPerWallet[msg.sender] += quantity;
+    }
+
+    // Withdraw to owner wallet
     function withdraw() external {
         uint256 balance = address(this).balance;
         require(balance > 0, "Insufficient balance in the contract");
         require(
             withdrawAddress !=
                 address(0x0000000000000000000000000000000000000000),
-            "withdraw address not set"
+            "Withdraw address not set"
         );
         payable(withdrawAddress).transfer(balance);
     }
@@ -111,7 +158,7 @@ contract Companion is ERC721Common, ReentrancyGuard, Ownable {
         require(
             withdrawAddress !=
                 address(0x0000000000000000000000000000000000000000),
-            "withdraw address not set"
+            "Withdraw address not set"
         );
         (bool sent, ) = withdrawAddress.call{value: balance}("");
         require(sent, "Failed to send Ether");
